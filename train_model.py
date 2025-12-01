e6a3eca5f56ac83361bd91157cc9be89832a27d5
pandurangchavan809 <pandurangchavan809@gmail.com>
2025-12-01 23:00:41 +0530

    chnages

--- train_model.py
+++ train_model.py
@@ -19,6 +19,6 @@ y = data['performance_score']
 model = LinearRegression()
 model.fit(X, y)
 
-# Save model as .pkl file in model folder
+# Save model as .pkl file
 joblib.dump(model, 'model/performance_model.pkl')
 print("✅ Model trained and saved as performance_model.pkl")
